8a8522dc89fc78ed5d8fc04fd54bbae67142d78c
madaansuraj12@gmail.com <madaansuraj12@gmail.com>
2021-09-22 12:10:49 +0300

    Add project files.

---
+++ ProjectManagementSystem/ProjectManagementSystem/Person.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectManagementSystem
+{
+    public class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int PersonID { get; set; }
+        public string Name { get; set; }
+        public string Passwrd { get; set; }
+    }
+}
---
+++ ProjectManagementSystem/ProjectManagementSystem/Project.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectManagementSystem
+{
+    public class Project
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ProjectID { get; set; }
+        public string Name { get; set; }
+       
+    }
+}
